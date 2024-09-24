88a9162f72be72f35acae3ecb1bba8fd6ea826f2
unknown <ngoumkwa.kenne@gmail.com>
2024-09-24 21:20:57 +0100

    Update HeroSection

--- src/pages/Home/components/HeroSection.jsx
+++ src/pages/Home/components/HeroSection.jsx
@@ -17,21 +17,23 @@ const HeroSection = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentBg((prevIndex) => (prevIndex + 1) % bgImages.length);
-    }, 5000); // Changer l'image toutes les 5 secondes
+    }, 7000); // Changer l'image toutes les 5 secondes
 
     return () => clearInterval(interval); // Nettoyage de l'intervalle lors du démontage
   }, [bgImages.length]);
 
   return (
     <div>
-    <div
-      className="h-screen flex items-center justify-center bg-cover bg-center transition-all duration-1000"
-      style={{ backgroundImage: bgImages[currentBg] }} // Image de fond dynamique
-    >
-      <div className="text-center space-y-4 w-1/2">
-        <h1 className="text-sm font-bold text-orange-500 uppercase">Installation et maintenance des panneaux solaires</h1>
-        <h2 className="text-4xl font-bold text-gray-600">Vous proposer des solutions sur mesure.</h2>
-        <p className="text-lg text-gray-500">
+   <div
+  className="h-screen flex items-center justify-center bg-cover bg-center transition-all duration-1000"
+  style={{
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), ${bgImages[currentBg]}`
+  }}
+>
+      <div className="text-center space-y-4 mx-5 lg:w-1/2">
+        <h1 className="text-sm font-bold text-orange-400 uppercase">Installation et maintenance des panneaux solaires</h1>
+        <h2 className="text-4xl font-bold text-white">Vous proposer des solutions sur mesure.</h2>
+        <p className="text-md text-gray-200">
           Nous offrons à nos clients, professionnels ou particuliers, un gage de qualité correspondant à leurs attentes et dans les plus brefs délais à travers tout le Cameroun.
         </p>
       </div>
